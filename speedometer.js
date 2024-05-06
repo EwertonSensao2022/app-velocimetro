f66a0469080095191ab1521d4da03495479618fe
ewerton henrique sensão <117674938+EwertonSensao2022@users.noreply.github.com>
2024-05-06 16:33:13 -0300

    ...

--- speedometer.js
+++ speedometer.js
@@ -1,18 +1,22 @@
-const speedElement = document.querySelector("speed")
+const speedElement = document.querySelector("#speed")
 const startBtn = document.querySelector("#start")
 const stopBtn = document.querySelector("#stop")
 
 let watchID = null
+
 startBtn.addEventListener("click",() => {
     if(watchID) 
      return
 
-    function handleSuccess() {
-        speendElement.innerText = position.coord.speed ? (position.coord.speed * 3.6).toFixed(1) : "0"
+
+    //  navigator.geolocation.getCurrentPosition(console.log)
+    function handleSuccess(position) {
+        console.log(position)
+        speedElement.innerText = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : "0"
     }
 
     function handleError(error){
-        console.log(error.ms);
+        alert(error)
     }
     const options = { ennableHighAccuracy: true }
     watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
@@ -22,11 +26,10 @@ startBtn.addEventListener("click",() => {
 })
 
 stopBtn.addEventListener("click",() => {
-    if(!watchID)
-        return
+    if(!watchID) return
 
     navigator.geolocation.clearWatch(watchID)
-    watchID == null    
+    watchID = null    
     startBtn.classList.remove("d-none")
     stopBtn.classList.add("d-none")
 })
